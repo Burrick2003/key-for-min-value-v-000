f8368bf5176f65f16eccf96c16e54ba13104e824
Blakeley Bond <burrick2003@yahoo.com>
2017-11-04 04:35:34 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   temp_key = {}
   temp_value = nil
   name_hash.each do |key, value|
-    if temp_value == nil || value < temp_value
+    if temp_value == {} || value < temp_value
       temp_key = key
       temp_value = value
       #output << key
